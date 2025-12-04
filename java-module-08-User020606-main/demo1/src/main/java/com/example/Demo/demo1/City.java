76eea531fd7d0f8a9de38c47f66ede65487f0690
User020606 <vincentmoua06@gmail.com>
2025-12-04 12:03:07 -0800

    Add files via upload

---
+++ java-module-08-User020606-main/demo1/src/main/java/com/example/Demo/demo1/City.java
@@ -0,0 +1,33 @@
+package com.example.Demo.demo1;
+
+
+
+public class City {
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    public City(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Lat: " + latitude + ", Lon: " + longitude + ")";
+    }
+}
